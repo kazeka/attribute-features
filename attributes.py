061207a77dd4c8289ee0f9abf5b5f41c0a3b1ce1
Alexander Kazeka <alexander.kazeka@gmail.com>
2014-11-17 20:04:57 +0300

    Refactored, updated to latest oct2py API

--- attributes.py
+++ attributes.py
@@ -1,9 +1,15 @@
+# coding:utf-8
+
 import cv2
 import numpy
 import oct2py
+import os
 
 def detect(fname):
-    with open("/Developer/litl/features/attribute_names.txt") as fp:
+    attribute_names = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                   "attribute_names.txt")
+
+    with open(attribute_names) as fp:
         cats = fp.readlines()
 
     cats = numpy.array([cat.strip() for cat in cats])
@@ -11,18 +17,20 @@ def detect(fname):
 
     oc = oct2py.Oct2Py()
     _ = oc.addpath(oc.genpath("./"))
-    _ = oc.run("pkg load image")
+    _ = oc.eval("pkg load image")
 
     codewords = oc.load_codewords()
     att_classifiers = oc.load_att_classifiers()
 
     im = oc.imread(fname)
-    apect_r = 1024.0 / im.shape[1]
-    dim = (1024, int(im.shape[0] * apect_r))
+    aspect_r = 1024.0 / im.shape[1]
+    dim = (1024, int(im.shape[0] * aspect_r))
 
-    resized = cv2.resize(im, dim, interpolation=cv2.INTER_AREA)
-    bbox = numpy.array([1, 1, resized.shape[1], resized.shape[0]])
-    pred = oc.extract_predictions(resized, bbox, att_classifiers, codewords)
+    if aspect_r > 1.:
+        im = cv2.resize(im, dim, interpolation=cv2.INTER_AREA)
+
+    bbox = numpy.array([1, 1, im.shape[1], im.shape[0]])
+    pred = oc.extract_predictions(im, bbox, att_classifiers, codewords)
 
     return sorted(zip(cats[pred > 0.25], pred[pred > 0.25]),
                   key=lambda x: x[1],
